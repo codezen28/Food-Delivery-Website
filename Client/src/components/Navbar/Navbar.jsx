dc9e1522d3361fb569582cf85d277264afc720c2
unknown <vaibhavgupta43911@gmail.com>
2024-05-13 23:47:22 +0530

    Navbar Ready

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css";
+import {assets} from "../../assets/assets";
 const Navbar = () => {
+  const [menu,setMenu]=useState("Home");
   return (
     <div className='navbar'>
-      
+      <img src={assets.logo} alt="" className='logo' />
+      <ul className='navbar-menu'>
+        <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+        <li  onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+        <li  onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-App</li>
+        <li  onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="/" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="/"/>
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
     </div>
   )
 }
